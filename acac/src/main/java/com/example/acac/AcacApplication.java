6cf44d389706982175c4202a684a1603d3db312b
Magnus Jonsson <jonsson.magnus@outlook.com>
2018-08-20 13:40:27 +0200

    project start

---
+++ acac/src/main/java/com/example/acac/AcacApplication.java
@@ -0,0 +1,12 @@
+package com.example.acac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcacApplication.class, args);
+    }
+}
