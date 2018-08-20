a811a024423498d97c23d6b0625184e7d4768fc3
Magnus Jonsson <jonsson.magnus@outlook.com>
2018-08-20 16:57:48 +0200

    kopierat från gamla

--- src/main/java/com/example/acac/MemberController.java
+++ src/main/java/com/example/acac/MemberController.java
@@ -1,4 +1,26 @@
 package com.example.acac;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MemberController {
+    @Autowired MemberRepository memberRepository;
+
+
+    @PostMapping ("/index")
+    public String registerMember (@RequestParam int ID,
+                                  @RequestParam int personalNumber,
+                                  @RequestParam String firstName,
+                                  @RequestParam String lastName,
+                                  @RequestParam String city,
+                                  @RequestParam String address,
+                                  @RequestParam int zipCode,
+                                  @RequestParam String eMail,
+                                  @RequestParam int phoneNumber,
+                                  @RequestParam String password) {
+        return "index";
+    }
 }
--- src/main/java/com/example/acac/PhotoController.java
+++ src/main/java/com/example/acac/PhotoController.java
@@ -1,5 +1,20 @@
 package com.example.acac;
 
-public class PhotoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class PhotoController {
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    @PostMapping ("/addphoto")
+    public String addphoto (@RequestParam int ID,
+                            @RequestParam String source,
+                            @RequestParam int accidentID) {
+        photoRepository.addPhoto(ID, source, accidentID);
+        return "home";
+    }
 }
