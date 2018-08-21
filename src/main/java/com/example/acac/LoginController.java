d53e6f12092dae4aa16e4b1cb06915a8d9468f15
matilda grannas <e.h.matilda@gmail.com>
2018-08-21 14:33:50 +0200

    struntkod

--- src/main/java/com/example/acac/LoginController.java
+++ src/main/java/com/example/acac/LoginController.java
@@ -1,7 +1,3 @@
-package com.example.acac;
-
-import com.example.acac.MemberRepository;
-import com.example.acac.Members;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,5 +11,41 @@ import java.util.List;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private MembersRepository membersRepository;
+
+
+
+    @GetMapping("/login")
+    public ModelAndView secret(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(true);
+        List<Members> members = membersRepository.getLoggdeInMembers();
+        if (session.getAttribute("loggedIn") != null) {
+
+            return new ModelAndView("redirect:home").addObject("members",members);
+
+        } else {
+
+            return new ModelAndView("login");
+        }
+    }
+    @PostMapping("/login")
+    public String postForm(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, HttpServletRequest request) {
+        boolean member = membersRepository.getMember(username, password);
+        if (member) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("loggedIn", true);
+
+            return "redirect:home";
+
+        } else {
+            return "login";
+        }
+
+    }
+
+
 
 }
