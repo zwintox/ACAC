82f02a80fc69591d84b4fa75bec8e2868211b69d
Maria Granholm <maria.granholm93@gmail.com>
2018-08-21 16:43:21 +0200

    halvfärdig loggedin-kod

--- src/main/java/com/example/acac/LoginController.java
+++ src/main/java/com/example/acac/LoginController.java
@@ -1,6 +1,3 @@
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 package com.example.acac;
 
 
@@ -9,41 +6,5 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
-
-    @Autowired
-    private MembersRepository membersRepository;
-
-
-
-    @GetMapping("/login")
-    public ModelAndView secret(HttpServletRequest request) {
-
-        HttpSession session = request.getSession(true);
-        List<Members> members = membersRepository.getLoggdeInMembers();
-        if (session.getAttribute("loggedIn") != null) {
-
-            return new ModelAndView("redirect:home").addObject("members",members);
-
-        } else {
-
-            return new ModelAndView("login");
-        }
-    }
-    @PostMapping("/login")
-    public String postForm(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, HttpServletRequest request) {
-        boolean member = membersRepository.getMember(username, password);
-        if (member) {
-            HttpSession session = request.getSession(true);
-            session.setAttribute("loggedIn", true);
-
-            return "redirect:home";
-
-        } else {
-            return "login";
-        }
-
-    }
-
-
 
 }
